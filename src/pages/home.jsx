32cf7f124c30fd39a483c078ac81a2e2666fd373
andrewNEW42 <sanjino56@gmail.com>
2022-03-24 18:31:34 +0200

    home section

---
+++ src/pages/home.jsx
@@ -0,0 +1,10 @@
+export const HomeSection = () => {
+    return (
+        <section>
+            <h1>Lorem Ipsum</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+            <button>Our specialists</button>
+            <button>Free trial</button>
+        </section>
+    )
+}
